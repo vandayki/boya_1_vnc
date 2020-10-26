f78c480882a4e6eadb469d58437c9d6241c4487f
vandayki <73494492+vandayki@users.noreply.github.com>
2020-10-26 19:45:05 +0100

    Create clone.sh

---
+++ src/ubuntu/booya/clone.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# cd /root/dring/
+a=10
+REPO_DIR=/headless/dring/
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+
+ echo "Bash version ${BASH_VERSION}..."
+ for i in {1..38..1}
+    do
+       cd /headless/dring/
+       echo "Welcome 0$i times"
+       if [ "$i" -ge "$a" ];
+       then
+       	c=$i;
+       else
+       	c=0$i;
+       fi
+       #git clone git@bitbucket.org:z0la1/$c.git
+       echo -e "\e[31mgit@bitbucket.org:z0la1/$c.git"
+       echo -e "\e[49m"
+       printf "I ${RED}love${NC} Stack Overflow\n"
+       cd $REPO_DIR$c
+       pwd
+       #sed -i "s/10919460/17686673/g" $REPO_DIR$c/bitbucket-pipelines.yml ## yac
+       sed -i "s/17686673/10919460/g" $REPO_DIR$c/bitbucket-pipelines.yml
+       echo "#" >> bitbucket-pipelines.yml
+       git add --all .
+       git commit -m "1Test comlmit"
+       git push git@bitbucket.org:z0la1/$c.git master
+ done
