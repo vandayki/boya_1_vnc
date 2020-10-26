bd2b46163704ba885a3050436fc03bdc8c853929
vandayki <73494492+vandayki@users.noreply.github.com>
2020-10-26 19:56:01 +0100

    Update clone.sh

--- src/ubuntu/booya/clone.sh
+++ src/ubuntu/booya/clone.sh
@@ -9,7 +9,7 @@ NC='\033[0m' # No Color
 
 
  echo "Bash version ${BASH_VERSION}..."
- for i in {1..38..1}
+ for i in {1..21..1}
     do
        cd /headless/dring/
        echo "Welcome 0$i times"
@@ -19,16 +19,16 @@ NC='\033[0m' # No Color
        else
        	c=0$i;
        fi
-       #git clone git@bitbucket.org:z0la1/$c.git
+       git clone git@bitbucket.org:z0la1/$c.git
        echo -e "\e[31mgit@bitbucket.org:z0la1/$c.git"
        echo -e "\e[49m"
        printf "I ${RED}love${NC} Stack Overflow\n"
        cd $REPO_DIR$c
        pwd
        #sed -i "s/10919460/17686673/g" $REPO_DIR$c/bitbucket-pipelines.yml ## yac
-       sed -i "s/17686673/10919460/g" $REPO_DIR$c/bitbucket-pipelines.yml
-       echo "#" >> bitbucket-pipelines.yml
-       git add --all .
-       git commit -m "1Test comlmit"
-       git push git@bitbucket.org:z0la1/$c.git master
+       #sed -i "s/17686673/10919460/g" $REPO_DIR$c/bitbucket-pipelines.yml
+      # echo "#" >> bitbucket-pipelines.yml
+       #git add --all .
+       #git commit -m "1Test comlmit"
+      # git push git@bitbucket.org:z0la1/$c.git master
  done
